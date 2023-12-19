a10aac940d6898cb6525cb5137bb6fc2cd337eb3
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2023-12-19 17:57:25 -0300

    updated to angular 17

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
+  imports: [],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../header/header.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, MatButtonModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
