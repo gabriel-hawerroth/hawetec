74db79ee911cf13894db4885b380c7bea26e68f5
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-11-24 16:36:07 -0300

    refact: use ngOptimizedImage

--- src/app/components/pages/home/components/profile-cards/profile-cards.component.ts
+++ src/app/components/pages/home/components/profile-cards/profile-cards.component.ts
@@ -1,10 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile-cards',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './profile-cards.component.html',
   styleUrl: './profile-cards.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileCardsComponent {}
+export class ProfileCardsComponent {
+  cloudFrontUrl = 'https://ddmv7vg76tda1.cloudfront.net';
+}
