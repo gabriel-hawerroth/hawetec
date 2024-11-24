d310726d8a319df95878068cc4eab7dc9bdbd0f0
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-11-24 17:06:43 -0300

    perf: images optimization

--- src/app/components/pages/home/components/profile-cards/profile-cards.component.ts
+++ src/app/components/pages/home/components/profile-cards/profile-cards.component.ts
@@ -11,4 +11,9 @@ import { cloudFrontUrl } from '../../../../../../shared/constants';
 })
 export class ProfileCardsComponent {
   cloudFrontUrl = cloudFrontUrl;
+
+  whatsImgLink = cloudFrontUrl + '/hawetec/whats.webp';
+  gmailImgLink = cloudFrontUrl + '/hawetec/gmail.webp';
+  linkedinImgLink = cloudFrontUrl + '/hawetec/linkedin.webp';
+  githubImgLink = cloudFrontUrl + '/hawetec/github.webp';
 }
