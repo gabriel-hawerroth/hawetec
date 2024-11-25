5159c4c974811af51aeb5c99b5db1124d190e11a
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-11-25 18:17:37 -0300

    feat: add button to add app to home screen

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,13 +1,61 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  PLATFORM_ID,
+  signal,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from '../../header/header.component';
 import { ProfileCardsComponent } from './components/profile-cards/profile-cards.component';
 import { ProjectButtonsComponent } from './components/project-buttons/project-buttons.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, ProfileCardsComponent, ProjectButtonsComponent],
+  imports: [
+    HeaderComponent,
+    ProfileCardsComponent,
+    ProjectButtonsComponent,
+    MatButtonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  private isBrowser: boolean;
+
+  private deferredPrompt: any = null;
+  showButton = signal(false);
+
+  constructor() {
+    this.isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      window.addEventListener('beforeinstallprompt', (event: any) => {
+        event.preventDefault(); // Evita o prompt automático
+        this.deferredPrompt = event; // Salva o evento para usar depois
+        this.showButton.set(true); // Exibe o botão
+      });
+    }
+  }
+
+  addToHomeScreen(): void {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt(); // Mostra o prompt
+      this.deferredPrompt.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('Usuário aceitou instalar o PWA');
+        } else {
+          console.log('Usuário recusou instalar o PWA');
+        }
+        this.deferredPrompt = null; // Reseta o prompt
+        this.showButton.set(false); // Oculta o botão
+      });
+    }
+  }
+}
