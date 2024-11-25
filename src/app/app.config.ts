7fe2d0f82a23546246a0bfdb0152cbba7b2e684a
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-11-25 00:01:13 -0300

    feat/perf: add app service-worker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
 import {
   ApplicationConfig,
+  isDevMode,
   provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
 
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
     provideClientHydration(),
     provideAnimations(),
   ],
